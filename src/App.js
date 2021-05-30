b36d144f4b195fb89c57ade5cb5f983c55e55766
mussinbenarbia <mouhssine.benarbia@gmail.com>
2021-05-30 16:08:13 +0900

    basic app skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useTypingGame from "react-typing-game-hook";
+import "./styles.css";
 
-function App() {
+const TypingGameDemo = () => {
+  let text = 'const person = {name:"Mussin", age:28};';
+  const {
+    states: {
+      charsState,
+      length,
+      currIndex,
+      currChar,
+      correctChar,
+      errorChar,
+      phase,
+      startTime,
+      endTime,
+    },
+    actions: { insertTyping, resetTyping },
+  } = useTypingGame(text, {
+    skipCurrentWordOnSpace: false,
+    pauseOnError: true,
+    countErrors: "everytime",
+  });
+
+  const handleKey = (key) => {
+    if (key === "Escape") {
+      resetTyping();
+    } else if (key.length === 1) {
+      insertTyping(key);
+    }
+    console.log({
+      charsState,
+      length,
+      currIndex,
+      currChar,
+      correctChar,
+      errorChar,
+      phase,
+      startTime,
+      endTime,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div
+        className="typing-test"
+        onKeyDown={(e) => {
+          handleKey(e.key);
+          e.preventDefault();
+        }}
+        tabIndex={0}>
+        {text.split("").map((char, index) => {
+          let state = charsState[index];
+          let color = state === 0 ? "" : state === 1 ? "green" : "red";
+          return (
+            <span
+              key={char + index}
+              style={{ color }}
+              className={currIndex + 1 === index ? "curr-letter" : ""}>
+              {"_"}
+            </span>
+          );
+        })}
+      </div>
+
+      <code className="snippet language-javascript">{text}</code>
     </div>
   );
-}
+};
 
-export default App;
+export default TypingGameDemo;
