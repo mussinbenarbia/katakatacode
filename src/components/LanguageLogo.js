e324aa3c9afed51a7a3bb61966ce92ec35068e4f
Mussin Benarbia <77260898+mussinbenarbia@users.noreply.github.com>
2021-06-20 23:24:04 +0900

    Update LanguageLogo.js

--- src/components/LanguageLogo.js
+++ src/components/LanguageLogo.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default function LanguageLogo({ language }) {
   return (
-    <img id="language-logo" src={require(`../images/${language}.svg`)} alt="" />
+    <img id="language-logo" src={require(`../images/${language}.svg`).default} alt="" />
   );
 }
